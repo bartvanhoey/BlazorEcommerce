4f304580478ddb64fec1f470bdf21d477cfc8f79
bartvanhoey <bartvanhoey@hotmail.com>
2023-10-20 10:53:37 +0200

    End 63

--- BlazorEcommerce/Server/Migrations/20231020085122_FeaturedProducts.Designer.cs
+++ BlazorEcommerce/Server/Migrations/20231020085122_FeaturedProducts.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Server.Data;
 
@@ -10,9 +11,11 @@ using Server.Data;
 namespace BlazorEcommerce.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231020085122_FeaturedProducts")]
+    partial class FeaturedProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -18,6 +18,7 @@ namespace BlazorEcommerce.Shared
         
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
+        public bool Featured { get; set; } = false;
 
         public List<ProductVariant> Variants { get; set; } = new();
 
