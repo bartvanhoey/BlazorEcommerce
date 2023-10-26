7f92c68dfa8f568db4998c38e319f9d8bbfab616
bartvanhoey <bartvanhoey@hotmail.com>
2023-10-26 14:33:27 +0200

    End 72

---
+++ BlazorEcommerce/Shared/CartItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public int ProductItemId { get; set; }
+    }
+}
