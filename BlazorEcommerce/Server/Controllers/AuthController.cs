6e0ebb2418b741bd6c92013ffc664a5ac57e1102
bartvanhoey <bartvanhoey@hotmail.com>
2023-11-08 10:57:43 +0100

    End 96

---
+++ BlazorEcommerce/Server/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Server.Services.Auth;
+using Shared;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterModel model)
+        {
+            var response = await _authService.Register(new User { Email = model.Email }, model.Password);
+
+            if (!response.Success) return BadRequest(response);
+            
+            return Ok(response);
+        }
+
+
+    }
+}
--- BlazorEcommerce/Shared/UserRegisterModel.cs
+++ BlazorEcommerce/Shared/UserRegisterModel.cs
@@ -6,7 +6,6 @@ namespace Shared
     {
 
         [Required, EmailAddress]
-
         public string Email { get; set; } = string.Empty;
 
         [Required]
@@ -14,10 +13,7 @@ namespace Shared
 
         public string Password { get; set; } = string.Empty;
 
-        // [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-
         [Compare("Password", ErrorMessage = "Passwords do not match.")]
-
         public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
