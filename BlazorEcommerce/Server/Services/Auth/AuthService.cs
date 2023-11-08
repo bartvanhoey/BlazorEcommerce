80d88124ccbab942ce52ff4a57c5599d824e7ce5
bartvanhoey <bartvanhoey@hotmail.com>
2023-11-08 10:28:39 +0100

    End 94

--- BlazorEcommerce/Server/Services/Auth/AuthService.cs
+++ BlazorEcommerce/Server/Services/Auth/AuthService.cs
@@ -5,22 +5,17 @@ namespace Server.Services.Auth
 {
     public class AuthService : IAuthService
     {
-        private readonly DatabaseContext databaseContext;
+        private readonly DatabaseContext _db;
 
-        public AuthService(DatabaseContext databaseContext)
-        {
-            this.databaseContext = databaseContext;
-        }
+        public AuthService(DatabaseContext databaseContext) => _db = databaseContext;
 
         public Task<ServiceResponse<int>> Register(User user, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> UserExists(string email)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<bool> UserExists(string email) 
+        => await _db.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower());
     }
 
 }
