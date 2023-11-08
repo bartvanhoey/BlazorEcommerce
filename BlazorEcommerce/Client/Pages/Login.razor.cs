4f6958f371f41710452155e79acf6b6f33d6b8d1
bartvanhoey <bartvanhoey@hotmail.com>
2023-11-08 16:33:20 +0100

    End 110

--- BlazorEcommerce/Client/Pages/Login.razor.cs
+++ BlazorEcommerce/Client/Pages/Login.razor.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace BlazorEcommerce.Client.Pages
 {
@@ -14,7 +15,7 @@ namespace BlazorEcommerce.Client.Pages
 
         [Inject] protected AuthenticationStateProvider? AuthenticationStateProvider  { get; set; }
 
-        
+        public string ReturnUrl { get; set; } = string.Empty;
 
         public string ErrorMessage { get; set; } = string.Empty;
 
@@ -26,12 +27,22 @@ namespace BlazorEcommerce.Client.Pages
                 ErrorMessage = string.Empty;
                 await LocalStorage!.SetItemAsync("authToken", result.Data);
                 await AuthenticationStateProvider?.GetAuthenticationStateAsync()!;
-                NavigationManager!.NavigateTo("/");
+                NavigationManager!.NavigateTo(ReturnUrl);
             }
             else
             {
                 ErrorMessage = result.Message;
             }
+        }
+
+        protected override void OnInitialized()
+        {
+            var uri = NavigationManager?.ToAbsoluteUri(NavigationManager.Uri);
+            if (uri != null && QueryHelpers.ParseQuery(uri.Query).TryGetValue("returnUrl", out var returnUrl))
+            {
+                ReturnUrl = returnUrl;
+            }
+
         }
     }
 }
