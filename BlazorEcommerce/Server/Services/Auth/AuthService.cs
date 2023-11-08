b9bfc0ab26ab0eb9cc5ed43055b00c8901018f35
bartvanhoey <bartvanhoey@hotmail.com>
2023-11-08 13:13:02 +0100

    End 104

--- BlazorEcommerce/Server/Services/Auth/AuthService.cs
+++ BlazorEcommerce/Server/Services/Auth/AuthService.cs
@@ -26,6 +26,42 @@ namespace Server.Services.Auth
             return new ServiceResponse<int> { Data = user.Id, Message = "Registration Successful!" };
         }
 
+
+        public async Task<bool> UserExists(string email)
+        => await _db.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower());
+
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var response = new ServiceResponse<string>();
+            response.Data = "token";
+
+            var user = await _db.Users.FirstOrDefaultAsync(u => u.Email.ToLower() == email.ToLower());
+
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "User not found";
+            }
+            else if (IsPasswordNotCorrect(password, user.PasswordHash, user.PasswordSalt))
+            {
+
+            }
+
+            await Task.CompletedTask;
+            return response;
+        }
+
+        private bool IsPasswordNotCorrect(string? password, byte[]? passwordHash, byte[]? passwordSalt)
+        {
+            if (password == null) return false;
+            if (passwordHash == null) return false;
+            if (passwordSalt == null) return false;
+
+            using var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt);
+            var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+
         private (byte[]? passwordHash, byte[]? passwordSalt) CreatePasswordHash(string password)
         {
             byte[]? passwordHash = null;
@@ -40,18 +76,6 @@ namespace Server.Services.Auth
             return (passwordHash, passwordSalt);
         }
 
-        public async Task<bool> UserExists(string email)
-        => await _db.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower());
-
-        public async Task<ServiceResponse<string>> Login(string email, string password)
-        {
-            var response = new ServiceResponse<string>{
-                Data = "token"
-            };
-
-            await Task.CompletedTask;
-            return response;
-        }
     }
 
 }
