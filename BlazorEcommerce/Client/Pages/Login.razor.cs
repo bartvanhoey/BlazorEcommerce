d3858c36acac1ee2a7d1559c3dc065f2a698a636
bartvanhoey <bartvanhoey@hotmail.com>
2023-11-08 11:57:37 +0100

    End 102

---
+++ BlazorEcommerce/Client/Pages/Login.razor.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Pages
+{
+    public class LoginBase :  ComponentBase
+    {
+        public UserLoginModel UserLoginModel { get; set; } = new();
+
+        protected async void HandleValidSubmitAsync()
+        {
+
+        
+        }
+    }
+}
