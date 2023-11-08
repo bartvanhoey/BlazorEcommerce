26e6f2b4a250a71fd7cb4a981c536f8eec82193b
bartvanhoey <bartvanhoey@hotmail.com>
2023-11-08 15:47:37 +0100

    End 107

---
+++ BlazorEcommerce/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,73 @@
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorEcommerce.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly HttpClient _http;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage, HttpClient httpClient)
+        {
+            _localStorage = localStorage;
+            _http = httpClient;
+        }
+
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var authToken = await _localStorage.GetItemAsync<string>("authToken");
+
+            var identity = new ClaimsIdentity();
+            _http.DefaultRequestHeaders.Authorization = null;
+            if (!string.IsNullOrWhiteSpace(authToken))
+            {
+                try
+                {
+                    identity = new ClaimsIdentity(ParseClaimsFromJwt(authToken), "jwt");
+                    _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken.Replace("\"", ""));
+                }
+                catch
+                {
+                    await _localStorage.RemoveItemAsync("authToken");
+                    identity = new ClaimsIdentity();
+                }
+            }
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+            return state;
+        }
+
+        private IEnumerable<Claim>? ParseClaimsFromJwt(string jwt)
+        {
+            List<Claim> claims = new();
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+            if (keyValuePairs != null && keyValuePairs.Count > 0)
+            {
+                claims.AddRange(keyValuePairs
+                      .Where(x => x.Value != null)
+                      .Select(kv => new Claim(kv.Key, kv.Value?.ToString())));
+            }
+            return claims;
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+
+    }
+}
