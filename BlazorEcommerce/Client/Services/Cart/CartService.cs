9e63ab676685fcaa2a62d810d714e3f8a8aa9ac1
bartvanhoey <bartvanhoey@hotmail.com>
2023-11-14 17:02:27 +0100

    End 122

--- BlazorEcommerce/Client/Services/Cart/CartService.cs
+++ BlazorEcommerce/Client/Services/Cart/CartService.cs
@@ -6,20 +6,31 @@ namespace Client.Services.Cart
 {
     public class CartService : ICartService
     {
-
         private readonly ILocalStorageService _localStorage;
         private readonly HttpClient _httpClient;
+        private readonly AuthenticationStateProvider _authenticationStateProvider;
 
-        public CartService(ILocalStorageService localStorageService, HttpClient httpClient)
+        public CartService(ILocalStorageService localStorageService, HttpClient httpClient, AuthenticationStateProvider authenticationStateProvider)
         {
             _localStorage = localStorageService;
             _httpClient = httpClient;
+            _authenticationStateProvider = authenticationStateProvider;
         }
 
         public event Action? OnChange;
 
         public async Task AddToCartAsync(CartItem cartItem)
         {
+            var isUserAuthenticated = (await _authenticationStateProvider.GetAuthenticationStateAsync()).User.Identity?.IsAuthenticated ?? false; ;
+            if (isUserAuthenticated)
+            {
+                Console.WriteLine("User already authenticated");
+            }
+            else
+            {
+                Console.WriteLine("User NOT authenticated");
+            }
+
             var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart") ?? new List<CartItem>();
             var sameItem = cart.FirstOrDefault(x => x.ProductId == cartItem.ProductId && x.ProductTypeId == cartItem.ProductTypeId);
             if (sameItem == null)
