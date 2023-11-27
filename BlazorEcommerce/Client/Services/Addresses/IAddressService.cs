66fcff3fd3d2c62acd443e99296a19fc58099d20
bartvanhoey <bartvanhoey@hotmail.com>
2023-11-27 17:16:49 +0100

    End 158

--- BlazorEcommerce/Client/Services/Addresses/IAddressService.cs
+++ BlazorEcommerce/Client/Services/Addresses/IAddressService.cs
@@ -10,23 +10,4 @@ namespace Client.Services.Addresses
         Task<Address> GetAddressAsync();
         Task<Address> AddOrdUpdateAddressAsync(Address address);
     }
-
-    public class AddressService : IAddressService
-    {
-        private readonly HttpClient _http;
-
-        public AddressService(HttpClient client) 
-            => _http = client;
-        public async Task<Address> AddOrdUpdateAddressAsync(Address address)
-        {
-            var response = await _http.PostAsJsonAsync("api/address", address);
-            return (await response.Content.ReadFromJsonAsync<ServiceResponse<Address>>()).Data;
-        }
-
-        public async Task<Address> GetAddressAsync()
-        {
-            var response = await _http.GetFromJsonAsync<ServiceResponse<Address>>("api/address");
-            return response.Data;
-        }
-    }
 }
